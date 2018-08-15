b5eb52d02abb70fe737c03fa4c795b9b00460355
coolguyethanw <42422230+coolguyethanw@users.noreply.github.com>
2018-08-15 12:26:15 -1000

    1st commit

---
+++ arrayadv.js
@@ -0,0 +1,12 @@
+var names = [];
+var prom =  prompt ("Whats your first name?");
+names[0]=[prom];
+var brompt = prompt ("Whats your last name");
+names[1]=[brompt];
+var ask = prompt ("Do you like anime?");
+window.alert(names[0] + " " + names[1]);
+window.alert(names.length);
+window.alert("null");
+window.alert("jk kys");
+
+
